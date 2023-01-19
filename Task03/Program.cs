e0c9dc66596bac4f4ffff2ddf8408937114884f2
Daria <dofedotova@mail.ru>
2023-01-19 22:46:10 +0300

    Task03

---
+++ Task03/Program.cs
@@ -0,0 +1,13 @@
+﻿// По заданному номеру дня недели вывести его название
+Console.WriteLine();
+string[] Days = {"Понедельник", "Втоник", "Среда", "Четверг", "Пятница", "Суббота", "Вскресенье"};
+
+Console.WriteLine("Введите номер дня недели: ");
+string.writeDays = Console.ReadLine();
+bool result = int.TryParse(writeDays, out int daysNumber); // определяет данные номера в день недели
+
+Console.WriteLine();
+if (result == false) Console.WriteLine("Число введено неверно");
+else if (0 < dayNumber && dayNumber < 8) System.Console.WriteLine($"{dayNumber}-й день недели - {Days[dayNumber - 1]}");
+else System.Console.WriteLine("Дня недели с таким номером не существует");
+System.Console.WriteLine();
