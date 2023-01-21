816905bc9f667d1d31233391878c51faea54e2ca
Daria <dofedotova@mail.ru>
2023-01-21 22:47:53 +0300

    Task09

---
+++ Task09/Program.cs
@@ -0,0 +1,17 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int Number = new Random().Next(10, 100);
+int Max = 0;
+int MaxDigit() // максимальная цифра
+{
+    int DigitOne = Number / 10;
+    int DigitTwo = Number % 10;
+    if (DigitOne > DigitTwo) Max = DigitOne;
+    else Max = DigitTwo;
+
+    return Max;
+}
+MaxDigit();
+Console.WriteLine();
+Console.WriteLine($" Вчисле {Number} наибольшая цифра {Max}");
+Console.WriteLine();
