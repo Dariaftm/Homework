39aff6431971da84d4101fad0e439d940cce9dc9
Daria <dofedotova@mail.ru>
2023-01-21 22:18:08 +0300

    Task07

---
+++ Task07/Program.cs
@@ -0,0 +1,6 @@
+﻿// Показать последнюю цифру трёхзначного числа
+Console.WriteLine();
+
+int number = new Random() .Next(100, 1000);
+Console.WriteLine($"Последней цифрой числа {number} является цифра {number%10}"); // {number%10} - выводит последнее число 
+Console.WriteLine();
