d8acbfb04a2aa7126bd6d00a0b5161312dec4491
Daria <dofedotova@mail.ru>
2023-01-23 22:02:46 +0300

    Исправлена ошибка в файле №2

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,10 +1,10 @@
 ﻿// По двум заданным числам проверять является ли первое квадратом второго
 Console.WriteLine("Введите первое число ");
-string writeNumber1 = Console.ReadLine();
+string writeNumber1 = Console.ReadLine()!;
 int number1 = Convert.ToInt32(writeNumber1); // команда, для перевода строки в число
 
 Console.WriteLine("Введите второе число: ");
-string writeNumber2 = Console.ReadLine();
+string writeNumber2 = Console.ReadLine()!;
 int number2 = Convert.ToInt32(writeNumber2);
 
 string txtResult = $"Число {number1} не является квадратом числа {number2} "; //  - выводим результат
