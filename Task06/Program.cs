b41ba264482d71cdc33fdfaeda4fd1aea28b335d
Daria <dofedotova@mail.ru>
2023-01-23 22:03:54 +0300

    Исправлена ошибка в файле №6

--- Task06/Program.cs
+++ Task06/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Показать четные числа от 1 до N
 Console.WriteLine("Введите число:  ");
-string txtN = Console.ReadLine();
+string txtN = Console.ReadLine()!;
 int endNum = Convert.ToInt32(txtN);
 int num = 0;
 Console.WriteLine($"Четные числа из диапазона от 1 до {endNum}:");
