07d376c6deb643331cc8582e40c0985cb47dd212
Daria <dofedotova@mail.ru>
2023-01-29 18:44:08 +0300

    Task04

---
+++ Homework_3/Task04/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру;
+Console.WriteLine();
+int N = new Random().Next(1, 30);
+Console.WriteLine($"N = {N}");
+
+int number = 1;
+while (number <= N);
+{
+    int cube = number * number * number;
+    if (cube % 2 == 0);
+    Console.WriteLine(cube + " ");
+    number++;
+}
+Console.WriteLine();
