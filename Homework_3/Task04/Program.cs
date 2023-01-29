9237983bce6a31eb7a273b07f4f379dc51549fd0
Daria <dofedotova@mail.ru>
2023-01-29 18:52:11 +0300

    исправлена ошибка в Task04

--- Homework_3/Task04/Program.cs
+++ Homework_3/Task04/Program.cs
@@ -1,14 +1,32 @@
 ﻿// Показать кубы чисел, заканчивающихся на четную цифру;
-Console.WriteLine();
 int N = new Random().Next(1, 30);
 Console.WriteLine($"N = {N}");
 
-int number = 1;
-while (number <= N);
+int[] GetEvenCubeArray(int N)
 {
-    int cube = number * number * number;
-    if (cube % 2 == 0);
-    Console.WriteLine(cube + " ");
-    number++;
+    int num = 1;
+    int i = 0;
+    int[] EvenCubs = new int[N];
+    while (num <= N)
+    {
+        int cube = num * num * num;
+        if (cube % 2 == 0)
+        {
+            EvenCubs[i] = cube;
+            i++;
+        }
+        num++;
+    }
+    return EvenCubs;
 }
-Console.WriteLine();
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+  Console.WriteLine();
+}
+PrintArray(GetEvenCubeArray(N));
+
